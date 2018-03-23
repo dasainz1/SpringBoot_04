79b4ecde0588f6ac583a497f078856645b3a1d0e
dasainz1 <dasainz@email.wm.edu>
2018-03-23 08:44:29 -0400

    Done

--- src/main/java/com/sainz3/demo/HomeController.java
+++ src/main/java/com/sainz3/demo/HomeController.java
@@ -8,16 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-}
     @RequestMapping ("/loadform")
     public String loadFormPage (){
     return "formtemplate";
     }
-
     @RequestMapping ("/processform")
     public String loadFromPage (@RequestParam("login")String login,
                                 Model model) {
-        model.addAttribute("loginval", login);
+        model.addAttribute ("loginval", login);
         return "confirm";
         }
     }
